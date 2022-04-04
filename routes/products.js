1f4c1208cfc2c536bb6a1d6b0f98b15a8faf1c4a
Antonio Rangel <arangelb11@gmail.com>
2022-04-04 18:51:20 -0400

    Created Get Products route

--- routes/products.js
+++ routes/products.js
@@ -2,5 +2,14 @@ var express = require("express");
 var router = express.Router();
 
 module.exports = function (db) {
+  router.get("/products", (req, res) => {
+    res.send(db.get("products").value());
+  });
+
+  router.post("/products", (req, res) => {
+    const newProduct = req.body;
+    res.send(db.get("products").insert(newProduct).write());
+  });
+
   return router;
 };
