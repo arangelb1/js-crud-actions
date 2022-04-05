2cc067b006981c31f0aedbe79ada33d7d4f2befd
Antonio Rangel <arangelb11@gmail.com>
2022-04-04 22:23:30 -0400

    Created other product routes

--- routes/products.js
+++ routes/products.js
@@ -11,5 +11,19 @@ module.exports = function (db) {
     res.send(db.get("products").insert(newProduct).write());
   });
 
+  router.patch("/products/:id", (req, res) => {
+    res.send(db.get("products".find({id: req.params.id})).assign(req.body).write());
+  });
+
+  router.delete("/products/:id", (req, res) => {
+    db.get("products".remove({id: req.params.id})).write();
+    res.status(204).send();
+  });
+
+  router.get("/products/:id", (req, res) => {
+    res.send(db.get("products".find({id: req.params.id})).value());
+  });
+
+
   return router;
 };
