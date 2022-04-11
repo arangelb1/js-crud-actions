997a426ccc05400a3ed521f6be76faa896cd2d6f
Antonio Rangel <arangelb11@gmail.com>
2022-04-11 19:10:44 -0400

    Search functionality

--- routes/products.js
+++ routes/products.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+const qs = require('qs');
 
 module.exports = function (db) {
   router
@@ -10,6 +11,41 @@ module.exports = function (db) {
     .post((req, res) => {
       const newProduct = req.body;
       res.send(db.get("products").insert(newProduct).write());
+    });
+
+
+    router.route('/products/search').get ((req, res) => {
+      const keywords = req.query.keywords;
+      const result = db.get("products").filter(_ => {
+        const fullText = _.description + _.name + _.color;
+        return fullText.indexOf(keywords) !== -1;
+      });
+      res.send(result);
+    });
+
+    router.route('/products/detailSearch').get((req, res) => {
+      const query = qs.parse(req.query);
+      const results = db.get("products").filter(_ => {
+        return Object.keys(query).reduce((found, key) => {
+          const obj = query[key];
+          // found = found && _[key] == obj.val;
+          switch(obj.op) {
+            case 'lt':
+              found = found && _[key] < obj.val;
+              break;
+            case 'eq':
+              found = found && _[key] == obj.val;
+              break;
+            default:
+              found = found && _[key].indexOf(obj.val) !== -1;
+              break;
+          };
+
+          return found;
+        }, true)
+      });
+
+      res.send(results);
     });
 
   router
@@ -36,14 +72,16 @@ module.exports = function (db) {
   //   res.send(db.get("products".find({id: req.params.id})).value());
   // });
 
-  router.get("/products/:id", (req, res) => {
-    const result = db.get("products".find({id: req.params.id})).value();
-      if (result) {
-        res.sendStatus(result);
-      } else {
-        res.status(404).send();
-      }
-  });
+  // router.get("/products/:id", (req, res) => {
+  //   const result = db.get("products".find({id: req.params.id})).value();
+  //     if (result) {
+  //       res.sendStatus(result);
+  //     } else {
+  //       res.status(404).send();
+  //     }
+  // });
+
+  
 
 
   return router;
